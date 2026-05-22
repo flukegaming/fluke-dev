73dfaeb8602f622e4de4edaad44efd31ca23861c
andi-74 <amsterling.dev@pm.me>
2026-05-21 18:21:34 -0600

    get live status from YT

--- resources/scripts/streams.js
+++ resources/scripts/streams.js
@@ -7,6 +7,8 @@ const tabs = {
 
 let twitchPlayer = null;
 
+// ---------- TWITCH STREAM ----------
+
 function loadTwitch() {
     container.innerHTML = ''; // clear YouTube iframe if present
 
@@ -20,17 +22,44 @@ function loadTwitch() {
     twitchPlayer.setVolume(0.5);
 }
 
-function loadYouTube() {
-  // Twitch embed doesn't have a clean destroy API, so wipe DOM
-  container.innerHTML = `
-    <iframe
-      src="const youtubeSrc =
-  "https://www.youtube.com/embed/live_stream?channel=UCewiYYWHvvmSqnxq5U9TNZg";"
-      allowfullscreen>
-    </iframe>
-  `;
-  twitchPlayer = null;
+// ---------- YOUTUBE STREAM ----------
+
+async function isYouTubeLive(channelId, apiKey) {
+    const base = 'https://www.googleapis.com/youtube/v3';
+
+    try {
+        const liveResponse = await fetch(
+            `${base}/search?part=snippet&channelId=${channelId}` +
+            `&eventType=live&type=video&key=${apiKey}`
+        );
+
+        const liveData = await liveResponse.json();
+        return (liveData.items && liveData.items.length > 0);
+
+    } catch (err) {
+      console.error('YT Live check failed:', err);
+      elStatus.querySelector('.stream__status-text').textContent =
+        'Could not load stream info.';
+    }
 }
+
+function loadYouTube() {
+    const API_KEY = 'AIzaSyBLAdU6OWt4djsbPXZGITUlkl6FYePXZ-w';
+    const CHANNEL_ID = 'UCewiYYWHvvmSqnxq5U9TNZg';
+
+    // Twitch embed doesn't have a clean destroy API, so wipe DOM
+    container.innerHTML = `
+        <iframe frameborder="0" modestbranding="1" mute="1" allowfullscreen
+        src="https://www.youtube.com/embed/live_stream?channel=${CHANNEL_ID}">
+        </iframe>
+    `;
+    twitchPlayer = null;
+    
+    var isLive = isYouTubeLive(CHANNEL_ID, API_KEY);
+    console.log(isLive);
+}
+
+// ---------- STREAM SELECTOR ----------
 
 function setActive(active) {
     Object.entries(tabs).forEach(([key, element]) => {
